82ad630721b4b4ae7709d6f28216bed13e0b09fc
hyeongyun <guddbs5545@naver.com>
2023-07-04 09:11:44 +0900

    first commit

---
+++ app/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('{ "response": "Hello From Thetips4you" }');
+});
+
+app.get('/will', function (req, res) {
+    res.send('{ "response": "Hello World" }');
+});
+app.get('/ready', function (req, res) {
+    res.send('{ "response": " Great!, It works!" }');
+});
+
+app.listen(8081);
+module.exports = app;
